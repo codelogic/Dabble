7d6001ee3d10df85684031f0b1ddaa0d56810362
Paul Rohde <paul@paulrohde.com>
2013-04-03 13:51:45 -0700

    Fixed the renderer by adding </html> at the end.

--- lib/renderer.dart
+++ lib/renderer.dart
@@ -2,13 +2,14 @@ part of dabble.core;
 
 class Renderer {
   String render({LanguageData markup, LanguageData style, LanguageData code}) {
-    String result = "<!doctype html>\n<html><head><style type=\"text/css\">"
-        + style.rawText
+    String result =
+        "<!doctype html>\n<html><head><style type=\"text/css\">"
+        + "${style.rawText}"
         + "</style></head><body>"
         + markup.rawText
         + "<script type=\"text/javascript\">"
         + code.rawText
-        + "</script></body>";
+        + "</script></body></html>";
 
     return result;
   }
