4b8b8d71e3ab8b020b7dc33f3e4895c14998b7b5
Paul Rohde <paul@paulrohde.com>
2013-04-03 14:09:56 -0700

    Adding a .gitignore and documenting api

--- lib/dabble_api.dart
+++ lib/dabble_api.dart
@@ -1,18 +1,18 @@
 part of dabble.core;
 
 abstract class DabbleApi {
-  /* create a persistant dabble instance populated with an id */
+  /** create a persistant dabble instance populated with an [dabbleId] */
   Future<ADabble> createNewDabble({owner: 'anonymous'});
-  
-  /** Gets a dabble by id. */
+
+  /** Gets a dabble by [dabbleId]. */
   Future<ADabble> getDabble(String dabbleId);
 
-  /* delete a dabble by passing in the id */
+  /** delete a dabble by passing in the [dabbleId] */
   Future deleteDabble(String dabbleId);
-  
-  /* update a dabble instance itself */
+
+  /** update a dabble via [dabbleId] instance with [newData] */
   Future<ADabble> insertNewVersion(String dabbleId, DabbleData newData);
-  
+
   /* when a particular dabble is updated */
   Stream<DabbleData> onUpdate(String dabbleId);
 }
