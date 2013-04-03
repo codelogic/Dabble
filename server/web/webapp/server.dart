5f3296f774f583a3cc46851e5ed6e5c706689b3f
Tony Payne <tpayne@dhcp-172-19-65-197.mtv.corp.google.com>
2013-04-03 16:56:39 -0700

    Added map for dart2js outputs

--- server/web/webapp/server.dart
+++ server/web/webapp/server.dart
@@ -8,6 +8,9 @@ void main() {
      '/': '/out/index.html',
      '/dabble.dart': '/out/dabble.dart',
      '/index.html_bootstrap.dart': '/out/index.html_bootstrap.dart',
+     '/index.html_bootstrap.dart.js': '/out/index.html_bootstrap.dart.js',
+     '/index.html_bootstrap.dart.js.deps': '/out/index.html_bootstrap.dart.js.deps',
+     '/index.html_bootstrap.dart.js.map': '/out/index.html_bootstrap.dart.js.map',
      '/dabble.dart.map': '/out/dabble.dart.map',
   };
   new StreamServer(uriMapping: map).start();
