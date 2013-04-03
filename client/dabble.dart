ea17cef3cec00db2b7888fe9fea8d134b27c14f1
Paul Rohde <paul@paulrohde.com>
2013-04-03 12:23:57 -0700

    More methods in dabble.dart

--- client/dabble.dart
+++ client/dabble.dart
@@ -1,11 +1,38 @@
 library dabble.dabble;
 
 import 'dart:html';
+import 'package:dabble/core.dart';
 
 void main() {
   query("#save")
-    .onClick.listen((_) => print("hello"));
-  
-  query("#htmlinput")
-    .onKeyUp.listen((event) => print(event.charCode));
+    .onClick.listen((_) => compileData.description);
 }
+
+DabbleData compileData() {
+  String name = query("#d-name").text;
+  String description = query("#d-description").text;
+  
+  DabbleData data = new DabbleData(
+      name: name,
+      description: description,
+      markup: markupLanguageBlob(),
+      style: styleLanguageBlob(),
+      applicationCode: appLanguageBlob());
+  
+  return data;
+}
+
+LanguageData markupLanguageBlob() => new LanguageData(
+      language: "html",
+      rawText: query("#htmlinput").value,
+      options: {});
+
+LanguageData styleLanguageBlob() => new LanguageData(
+      language: "css",
+      rawText: query("#cssinput").value,
+      options: {});
+
+LanguageData appLanguageBlob() => new LanguageData(
+      language: "js",
+      rawText: query("#jsinput").value,
+      options: {});
