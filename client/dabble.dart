8bb9e4525e0d698baa6e26a3fe8c655c660ff915
Paul Rohde <paul@paulrohde.com>
2013-04-03 11:01:39 -0700

    Alert on click

--- client/dabble.dart
+++ client/dabble.dart
@@ -1,6 +1,12 @@
 part of dabble.client;
 
 void main() {
-  query("#sample_text_id")
-    ..text = "Click me!";
+  query("#save")
+    .onClick
+    .listen((event) => Window.alert("WIN!"));
+  
+  var subscription = query("#save").onClick.listen(
+      (event) => print('click!'));
+
+  subscription.cancel();
 }
