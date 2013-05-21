ef0d7c07af9e4c5a65a3e5818b68166db446dc29
Tony Payne <tpayne@dhcp-172-23-245-195.lax.corp.google.com>
2013-05-21 16:18:49 -0700

    fix build.dart

--- build.dart
+++ build.dart
@@ -7,22 +7,24 @@ void main() {
   WEBUI.build(options.arguments, [
     'server/web/index.html',
     'server/web/editorComponent.html',
-    'server/web/live.html']);
-  String dir = path.dirname(options.executable);
-  Process.run("$dir/dart2js",
-      ["-oserver/web/out/dabble.dart.js",
-       "server/web/out/dabble.dart"]);
-  Process.run("$dir/dart2js",
-      ["-oserver/web/out/live.dart.js",
-       "server/web/out/live.dart"]);
-  Process.run("$dir/dart2js",
-      ["-oserver/web/out/index.html_bootstrap.dart.js",
-       "server/web/out/index.html_bootstrap.dart"]);
-  Process.run("$dir/dart2js",
-      ["-oserver/web/out/live.html_bootstrap.dart.js",
-       "server/web/out/live.html_bootstrap.dart"]);
-  Process.run("$dir/dart2js",
-      ["-oserver/web/out/editorComponent.html_bootstrap.dart.js",
-       "server/web/out/editorComponent.html_bootstrap.dart"]);
+    'server/web/live.html']).then((_) {
+        String dir = path.dirname(options.executable);
+      print("$dir");
+    Process.run("$dir/dart2js",
+        ["-oserver/web/out/dabble.dart.js",
+         "server/web/out/dabble.dart"]);
+    Process.run("$dir/dart2js",
+        ["-oserver/web/out/live.dart.js",
+         "server/web/out/live.dart"]);
+    Process.run("$dir/dart2js",
+        ["-oserver/web/out/index.html_bootstrap.dart.js",
+         "server/web/out/index.html_bootstrap.dart"]);
+    Process.run("$dir/dart2js",
+        ["-oserver/web/out/live.html_bootstrap.dart.js",
+         "server/web/out/live.html_bootstrap.dart"]);
+    Process.run("$dir/dart2js",
+        ["-oserver/web/out/editorComponent.html_bootstrap.dart.js",
+         "server/web/out/editorComponent.html_bootstrap.dart"]);
+  });
 }
 
